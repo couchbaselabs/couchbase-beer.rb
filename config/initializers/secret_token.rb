76c3d45b210996a509ba1e896ec406224705bdbb
Sergey Avseyev <sergey.avseyev@gmail.com>
2012-08-18 21:27:03 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CouchbaseBeer::Application.config.secret_token = '5962ce84f5263d7b2998a2ddbf9482f94c685bb774225b24d12cd2f3974b753789d70b9102a5c0b5cc9570ea6ad726b7137d8f3aa0e5c8fe676b9851a17e37a4'

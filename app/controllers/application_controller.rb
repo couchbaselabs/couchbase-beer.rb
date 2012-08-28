ac236dd2a62fda7c5fddfbf6dbf99d14bbaab0d3
Sergey Avseyev <sergey.avseyev@gmail.com>
2012-08-28 12:40:52 +0300

    Add caching to couchbase

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  after_filter do
+    expires_in 1.hour, :public => true
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,7 @@ module CouchbaseBeer
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.cache_store = :couchbase_store, {:bucket => 'cache', :expires_in => 3600}
   end
 end

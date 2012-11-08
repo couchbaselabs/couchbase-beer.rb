2c8e3da7a1bfc250c60084c2942d7284a66ab796
Sergey Avseyev <sergey.avseyev@gmail.com>
2012-11-08 18:33:05 +0300

    Note about session store
    Change-Id: Ib9c9d57e35fe0e12bdb365c84c7ab76377f2409a

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,3 +6,10 @@ CouchbaseBeer::Application.config.session_store :cookie_store, :key => '_couchba
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # CouchbaseBeer::Application.config.session_store :active_record_store
+
+# Use the Couchbase for sessions instead of the cookie-based default,
+# Don't forget to create the bucket
+#
+# require 'action_dispatch/middleware/session/couchbase_store'
+# config.session_store :couchbase_store, :namespace => "beer:", :couchbase => {:bucket => 'session'}
+

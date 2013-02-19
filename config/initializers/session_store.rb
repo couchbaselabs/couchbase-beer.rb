912848cda7ba545e426d9aab069a7b68bd305988
Sergey Avseyev <sergey.avseyev@gmail.com>
2013-02-19 12:56:12 +0300

    Do not use couchbase session store by default
    Because it has to be configured separately from config/couchbase.yml
    and people might forget to change hostname there for example
    
    Change-Id: I9a229e22c3a842a73972d55ff0f98a1cfe2a16ff

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-# CouchbaseBeer::Application.config.session_store :cookie_store, :key => '_couchbase-beer_session'
+CouchbaseBeer::Application.config.session_store :cookie_store, :key => '_couchbase-beer_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
@@ -10,7 +10,7 @@
 # Use the Couchbase for sessions instead of the cookie-based default,
 # Don't forget to create the bucket
 #
-require 'action_dispatch/middleware/session/couchbase_store'
-CouchbaseBeer::Application.config.session_store(:couchbase_store, :namespace => "session:",
-                                                :couchbase => {:bucket => 'beer-sample', :default_format => :marshal})
+# require 'action_dispatch/middleware/session/couchbase_store'
+# CouchbaseBeer::Application.config.session_store(:couchbase_store, :namespace => "session:",
+#                                                 :couchbase => {:bucket => 'beer-sample', :default_format => :marshal})
 
